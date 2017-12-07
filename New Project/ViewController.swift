d9e01f2ecffa7f28e3108d179b179188fadd180b
Giuseppe Bosco <mtlfkr@gmail.com>
2017-12-07 09:51:14 +0100

    Initial Commit

---
+++ New Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  New Project
+//
+//  Created by Giuseppe Bosco on 07/12/17.
+//  Copyright © 2017 Giuseppe Bosco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
